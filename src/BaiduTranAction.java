48c416a1f1acb860da3f8e0fd1588c4cb75a7079
liangshan <liang-shan@outlook.com>
2017-04-20 21:30:14 +0800

    气泡

--- src/BaiduTranAction.java
+++ src/BaiduTranAction.java
@@ -6,8 +6,13 @@ import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.BalloonBuilder;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.ui.JBColor;
 
+import java.awt.*;
 import java.util.List;
 
 /**
@@ -15,44 +20,47 @@ import java.util.List;
  */
 public class BaiduTranAction extends AnAction {
 
-    private Application application=null;
+    private JBPopupFactory jbPopupFactory = JBPopupFactory.getInstance();
+    private Application application = null;
     private Editor editor;
-    private BaiduTran baiduTran=null;
+    private BaiduTran baiduTran = null;
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-        if(application==null){
-            application= ApplicationManager.getApplication();
+        if (application == null) {
+            application = ApplicationManager.getApplication();
         }
         //获取编辑器
-        editor=e.getData(PlatformDataKeys.EDITOR);
+        editor = e.getData(PlatformDataKeys.EDITOR);
         //获取选择的文字
         String selectText = editor.getSelectionModel().getSelectedText();
-        if(StringUtil.isNotEmpty(selectText)){
-            if(baiduTran==null){
-                baiduTran=application.getComponent(BaiduTran.class);
+        if (StringUtil.isNotEmpty(selectText)) {
+            if (baiduTran == null) {
+                baiduTran = application.getComponent(BaiduTran.class);
             }
             tranResult(baiduTran.query(selectText));
         }
     }
 
-    private void tranResult(String str){
-        Gson gson=new Gson();
-        AllResult result=gson.fromJson(str,AllResult.class);
-        StringBuffer buffer=new StringBuffer();
-        for(TransResult transResult:result.trans_result){
-            buffer.append(transResult.src+"  :  "+transResult.dst+"\n");
+    private void tranResult(String str) {
+        Gson gson = new Gson();
+        AllResult result = gson.fromJson(str, AllResult.class);
+        StringBuffer buffer = new StringBuffer();
+        for (TransResult transResult : result.trans_result) {
+            buffer.append(transResult.src + "  :  " + transResult.dst + "\n");
         }
-
-        Messages.showInfoMessage(buffer.toString(),"翻译结果");
+        BalloonBuilder balloonBuilder = jbPopupFactory.createHtmlTextBalloonBuilder(buffer.toString(), null,
+                new JBColor(new Color(188, 238, 188), new Color(73, 120, 73)), null);
+        balloonBuilder.setFadeoutTime(5000).createBalloon().show(jbPopupFactory.guessBestPopupLocation(editor), Balloon.Position.below);
     }
 
-    class AllResult{
+    class AllResult {
         String from;
         String to;
         List<TransResult> trans_result;
     }
 
-    class TransResult{
+    class TransResult {
         String src;
         String dst;
     }
